c4be63b3698d6e4c14a55330ca5265d2c05a4957
einbocha <65666391+einbocha@users.noreply.github.com>
2026-02-18 17:26:05 +0100

    Deprecated (finalize) visualization of the java integrated gc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        A a = new A();
+        a = null; // memory of A() now unreachable
+        System.gc(); // optional since the gc is invoked periodically
+        while (true);
+    }
+}
+
+
+class A {
+    public void finalize() {
+        System.out.println("finalize");
     }
 }

21dfe9ff64dc5f2bed8c5a9181f4a17ac1c8faf1
einbocha <65666391+einbocha@users.noreply.github.com>
2026-03-11 02:35:17 +0100

    Introduce multithreading with custom Thread and Runnable implementations

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
 public class Main {
-    public static void main(String[] args) {
-        A a = new A();
-        a = null; // memory of A() now unreachable
-        System.gc(); // optional since the gc is invoked periodically
-        while (true);
+    public static void main(String[] args) throws InterruptedException {
+        int cores = Runtime.getRuntime().availableProcessors();
+
+        System.out.println("Cores: " + cores);
+
+        var t = new MyT(10);
+        var op = new MyTt("lolbert");
+        var titus = new Thread(op);
+        t.start();
+        titus.start();
+        t.join(); // main waits until 't' returned from its run method - comment: both wait simultaneously
+        titus.join(); // join order is irrelevant IF both were started before
+        Thread.sleep(3000);
+        System.out.println("Main finished");
     }
 }
 
+class MyTt implements Runnable {
+    String lolita;
 
-class A {
-    public void finalize() {
-        System.out.println("finalize");
+    public MyTt(String passme) {
+        this.lolita = passme + "postfix";
+    }
+
+    @Override
+    public void run() {
+        System.out.println(lolita + " started");
+
+        try {
+            Thread.sleep(6500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(lolita + " finished");
+    }
+}
+
+class MyT extends Thread {
+    int prop;
+
+    public MyT(int prop) {
+        this.prop = prop;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Prop: " + prop);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("finished waiting");
     }
 }
